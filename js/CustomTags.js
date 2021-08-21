1dbd1bc6de646b4712877210fd19ea64df044766
EtashTyagi <etash18@gmail.com>
2021-08-21 15:13:02 +0530

    login + other minor front end

--- js/CustomTags.js
+++ js/CustomTags.js
@@ -1,12 +1,39 @@
 <!-- path wrt files where html is located-->
+
 class NavHeader extends HTMLElement {
     connectedCallback() {
         let attributes = this.attributes;
         const active = {Home: (attributes.Home?"active":""),
                         Discounts: (attributes.Discounts?"active":""),
-                        Sell: (attributes.Sell?"active":"")}
+                        Sell: (attributes.Sell?"active":""),
+                        Cart: (attributes.Cart?"btn-outline-success":"btn-dark"),
+                        Login: (attributes.Login?"btn-outline-success ps-1":"btn-dark ps-0")}
+        let search=`<div class="w-100"></div>`;
+        if (attributes.Search) {
+            search=`
+<form class="d-flex mx-0 mx-md-3 w-100">
+    <div class="dropdown">
+        <a class="rounded-0 rounded-start btn btn-success dropdown-toggle" href="#" id="type"
+           role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <span id="selected_item_type">All</span>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="type">
+            <li><a class="dropdown-item" href="#All">All</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#Electronics">Electronics</a></li>
+            <li><a class="dropdown-item" href="#Laptop">Laptop</a></li>
+        </ul>
+    </div>
+    <input class="form-control rounded-0 border-0" type="search" placeholder="Search"
+           aria-label="Search">
+    <button class="rounded-0 rounded-end btn btn-success" type="submit">Search</button>
+</form>
+            `
+        }
         this.innerHTML = `
-<nav class="shadow-sm navbar navbar-expand-md navbar-dark bg-dark position-absolute w-100">
+<nav class="shadow-sm navbar navbar-expand-md navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="./home.html">
         <div class="d-flex align-items-center justify-content-center">
@@ -29,29 +56,24 @@ class NavHeader extends HTMLElement {
           <a class="nav-link ${active.Sell}" href="./sell.html">Sell</a>
         </li>
       </ul>
-      <form class="d-flex mx-0 mx-md-3 w-100">
-        <div class="dropdown">
-          <a class="rounded-0 rounded-start btn btn-success dropdown-toggle" href="#" id="type" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <span id="selected_item_type">All</span>
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="type">
-            <li><a class="dropdown-item" href="#All">All</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#Electronics">Electronics</a></li>
-            <li><a class="dropdown-item" href="#Laptop">Laptop</a></li>
-          </ul>
-        </div>
-        <input class="form-control rounded-0 border-0" type="search" placeholder="Search" aria-label="Search">
-        <button class="rounded-0 rounded-end btn btn-success" type="submit">Search</button>
-      </form>
-      <div class="nav-item dropdown pt-2 pt-md-0">
-        <a class="dropdown-toggle btn btn-dark link-primary ps-0 ps-md-2" href="#" id="login" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <img class="rounded-circle" height="28px" src="https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif" alt="User"/>
-        </a>
+      ${search}      
+      <div class="d-flex flex-row me-auto mb-2 mb-md-0">
+        <div class="nav-item dropdown pt-2 pt-md-0">
+            <a class="dropdown-toggle btn ${active.Login} link-primary ps-md-2" href="#" id="login" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <img class="rounded-circle" height="28px" src="https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif" alt="User"/>
+            </a>
         <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-md-end" aria-labelledby="login">
           <li><a class="dropdown-item" href="./login.html">Login</a></li>
           <li><a class="dropdown-item" href="./login.html">Signup</a></li>
         </ul>
+        </div>
+        <div class="nav-item dropdown pt-2 pt-md-0 ms-1">
+            <div class="btn ${active.Cart}">
+                <a href="./cart.html" id="login" role="button" aria-expanded="false">
+                    <img class="rounded-circle" height="28px" src="./res/cartIcon.png" alt="Cart"/>
+                </a>  
+            </div>
+        </div>
       </div>
     </div>
   </div>
@@ -64,24 +86,26 @@ class Footer extends HTMLElement {
         let attributes = this.attributes;
         const active = {Home: (attributes.Home?"active":""),
             Discounts: (attributes.Discounts?"active":""),
-            Sell: (attributes.Sell?"active":"")}
+            Sell: (attributes.Sell?"active":""),
+            Login: (attributes.Login?"active":"")}
         this.innerHTML=`
 <nav class="shadow-sm navbar navbar-dark bg-dark">
   <div class="w-100">
     <div class="d-flex flex-column">
       <span class="text-success ms-2">© FCS Group, 2021</span>
-      <ul class="mx-2 navbar-nav d-flex flex-row justify-content-around">
-        <li class="nav-item">
+      <rect class="mt-2 w-100 bg-warning" style="height: 1px"></rect>
+      <ul class="mt-2 mx-2 navbar-nav d-flex flex-row justify-content-around flex-wrap">
+        <li class="nav-item text-center" style="min-width: 150px">
           <a class="nav-link ${active.Home}" aria-current="page" href="./home.html">Home</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item text-center" style="min-width: 150px">
           <a class="nav-link ${active.Discounts}" href="./discounts.html">Discounts</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item text-center" style="min-width: 150px">
           <a class="nav-link ${active.Sell}" href="./sell.html">Sell</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./login.html">Login</a>
+        <li class="nav-item text-center" style="min-width: 150px">
+          <a class="nav-link ${active.Login}" href="./login.html">Login</a>
         </li>
       </ul>
     </div>
