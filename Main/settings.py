41b64da72014d4d20bb2e844570261c290884704
EtashTyagi <etash18@gmail.com>
2021-10-15 04:00:25 +0530

    client side validation + hints

--- Main/settings.py
+++ Main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--pyusjbuve0rh^&*9j-nd)l55sw67l$@^hfy-!1c7zz--ol7s&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.10"]
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.10", "etash.com"]
 
 
 # Application definition
