e0d13179a010f6b7e3211988b9a98e009c51d8d0
EtashTyagi <etash18@gmail.com>
2021-10-20 02:11:11 +0530

    Fixed some bugs.

--- Main/fetch_items.py
+++ Main/fetch_items.py
@@ -1,24 +1,47 @@
 # TODO: Number of available items, use in cart and while displaying on main tab
-# IMPORTANT: PLEASE DO NOT USE FORMAT STRING IN RAW SQL QUERIES, IT WILL CAUSE SQL INJECTIONS https://docs.djangoproject.com/en/3.2/topics/db/sql/
+# IMPORTANT: PLEASE DO NOT USE FORMAT STRING IN RAW SQL QUERIES, IT WILL CAUSE SQL INJECTIONS:
+# https://docs.djangoproject.com/en/3.2/topics/db/sql/
 import json
 import math
 import random
 from Main.models import Product
 
-
 # Return None for invalid request
+"""
+    Params in request (method == GET)
+        1. q -> search query
+"""
+
+
 def fetchItems(request):
     # TODO params like sort by, name, etc...
     result_fetch = []
-    for prod in Product.objects.raw('SELECT 1 id, ID, title, image, short_description, price FROM products'):
-        result_fetch.append({
-                'ID': str(prod.ID),
-                'image': prod.image,
-                'title': prod.title,
-                'short_description': prod.short_description,
-                'price': str(prod.price),
-                'rating': str(int(random.random() * 11))
-            })
+    q_string = ('%' + ("" if "q" not in request.GET else request.GET["q"][:-1]
+    if request.GET["q"][-1] == '/' else request.GET["q"]) + '%').lower()
+    sh_item = lambda product: {'ID': str(prod.ID),
+                               'image': prod.image,
+                               'title': prod.title,
+                               'short_description': prod.short_description,
+                               'price': str(prod.price),
+                               'rating': str(int(random.random() * 11))}
+    done_ids = set({})
+    for prod in Product.objects.raw(
+            'SELECT 1 id, ID, title, image, short_description, price FROM products WHERE LOWER(title) LIKE %s', [q_string]):
+        result_fetch.append(sh_item(prod))
+        done_ids.add(prod.ID)
+
+    for prod in Product.objects.raw(
+            'SELECT 1 id, ID, title, image, short_description, price FROM products WHERE LOWER(short_description) LIKE %s', [q_string]):
+        if prod.ID not in done_ids:
+            result_fetch.append(sh_item(prod))
+            done_ids.add(prod.ID)
+
+    for prod in Product.objects.raw(
+            'SELECT 1 id, ID, title, image, short_description, price FROM products WHERE LOWER(description) LIKE %s', [q_string]):
+        if prod.ID not in done_ids:
+            result_fetch.append(sh_item(prod))
+            done_ids.add(prod.ID)
+
     return result_fetch
 
 
--- Main/views.py
+++ Main/views.py
@@ -3,54 +3,71 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
-from Main.cart_handler import fetchCart, is_in_cart, add_to_cart
+from Main.cart_handler import fetchCart, is_in_cart, add_to_cart, remove_from_cart
 from Main.fetch_items import *
 from Main.auth_handler import *
 
 """ Define All HTML Views To Render Here in all_views list with appropriate name"""
+""" View Format (Follow Strictly !!!!):
+        0. Handle invalid requests (eg in item.)
+        1. Handle tasks dependent on auth of user (only authed user can do some tasks)
+        2. Handle permissions (only seller can sell)
+        3. Handle GET request
+        4. Handle POST request
+        5. Else return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
+        6. When combined (POST and must be authed), handle auth inside the request type block !!
+        7. After change to database using POST 'ALWAYS' redirect !!!
+"""
 
-
-#search title
-def search(request):
-    payload = []
-    # payload
-    
-    for prod in Product.objects.raw('SELECT 1 id,  title FROM products'):
-        payload.append(prod.title)
-
-    # print(request.GET)
-    filtered_list = [tup for tup in payload if request.GET['title'].lower() in tup.lower()]
-
-    # unzipped_object = zip(*filtered_list)
-    # unzipped_list = list(unzipped_object )
-
-    return JsonResponse({'status' : 200 , 'data':filtered_list})
 
 def home(request):
-    if request.method == "POST":
-        return HttpResponse("404 - not found")
+    request.session.pop('login_to_continue_to', None)
+    if request.method == "GET":
+        args = {"items": fetchItems(request), "q": ("" if "q" not in request.GET else request.GET["q"][:-1]
+        if request.GET["q"][-1] == '/' else request.GET["q"])}
+        return render(request, 'pages/home.html', args)
+    else:
+        return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
 
-    args = {"items": fetchItems(request)}
-    return render(request, 'home.html', args)
+
+# SEARCH HANDLER
+def search(request):
+    MAX_RES = 10
+    if request.method == "GET":
+        payload = fetchItems(request)
+        return JsonResponse({'status': 200, 'data': payload[0:min(MAX_RES, len(payload))]})
+    else:
+        return HttpResponse("<h1>Error</h1><p>Bad Request, only GET allowed!</p>")
 
 
 def cart(request):
-    if request.method == "POST":
-        return HttpResponse("404 - not found")
-
-    if request.user.is_authenticated:
-        args = {"items": fetchCart(request.user)}
-        return render(request, 'cart.html', args)
-    else:
-        request.session['login_to_continue'] = True
+    request.session.pop('login_to_continue_to', None)
+    args = {"items": fetchCart(request.user)}
+    if not request.user.is_authenticated:
+        request.session['login_to_continue_to'] = "/cart"
         return redirect('/login/')
+    elif request.method == "GET":
+        return render(request, 'pages/cart.html', args)
+    elif request.method == "POST":
+        if "remove_from_cart" in request.POST:
+            removal = request.POST["remove_from_cart"]
+            if removal.isdigit() and is_in_cart(request.user, int(removal)):
+                remove_from_cart(request.user, int(removal))
+                return redirect('/cart/')
+            else:
+                return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
+        else:
+            return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
+    else:
+        return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
 
 
+# FIXME: This is redundant?
 def discounts(request):
-    if request.method == "POST":
-        return HttpResponse("404 - not found")
-
-    return render(request, 'discounts.html')
+    if request.method == "GET":
+        return render(request, 'pages/discounts.html')
+    else:
+        return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
 
 
 def login(request):
@@ -58,77 +75,118 @@ def login(request):
     if request.user.is_authenticated:
         return redirect('/')
 
-    if request.method == "POST":
+    elif request.method == "GET":
+        if 'redirect_from_signup' in request.session.keys():
+            args["green"] = True
+            args["message"] = "Sign-up Successful"
+            request.session.pop('redirect_from_signup')
+        if 'login_to_continue_to' in request.session.keys():
+            args["green"] = True
+            args["message"] = "Log-in To Continue"
+        return render(request, 'pages/login.html', args)
+    elif request.method == "POST":
         response = authenticate_user(request)
         if not response[0]:
             args["red"] = True
             args["message"] = "Wrong Credentials"
+            return render(request, 'pages/login.html', args)
         else:
-            return redirect('/')
-    if 'redirect_from_signup' in request.session.keys():
-        args["green"] = True
-        args["message"] = "Sign-up Successful"
-        request.session.pop('redirect_from_signup')
-    if 'login_to_continue' in request.session.keys():
-        args["green"] = True
-        args["message"] = "Log-in To Continue"
-        request.session.pop('login_to_continue')
-
-    return render(request, 'login.html', args)
+            redirect_to = "/"
+            if 'login_to_continue_to' in request.session.keys():
+                redirect_to = request.session['login_to_continue_to']
+            return redirect(redirect_to)
+    else:
+        return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
 
 
+# TODO : SELLER PERMISSION
 def sell(request):
-    if request.method == "POST":
-        return HttpResponse("404 - not found")
+    request.session.pop('login_to_continue_to', None)
+    if not request.user.is_authenticated:
+        request.session['login_to_continue_to'] = "/sell"
+        return redirect('/login/')
+    elif request.method == "GET":
+        return render(request, 'pages/sell.html')
+    elif request.method == "POST":
+        return request("<h1>Error</h1><p>Not Implemented</p>")
+    else:
+        return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
 
-    return render(request, 'sell.html')
 
-
+# TODO: VERIFY EMAIL?
 def signup(request):
-    args = {"failed": False}
+    request.session.pop('login_to_continue_to', None)
+    args = {"failed": False, "message": ""}
     if request.user.is_authenticated:
         return redirect('/')
-
-    if request.method == "POST":
+    elif request.method == "GET":
+        return render(request, 'pages/signup.html', args)
+    elif request.method == "POST":
         response = create_user(request)
         if not response[0]:
             args["failed"] = True
-            args["message"] = response[1]
-            return render(request, 'signup.html', args)
+            return render(request, 'pages/signup.html', args)
         else:
             args["user"] = response[1]
-            request.session['redirect_from_signup'] = True
-            return redirect('/login/')
-
-    return render(request, 'signup.html', args)
+        request.session['redirect_from_signup'] = True
+        return redirect('/login/')
+    else:
+        return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
 
 
 def item(request):
-    if request.method == "POST":
-        if request.POST["type"] == "add_to_cart":
-            product_id = request.GET["id"][:-1] if request.GET["id"][-1] == '/' else request.GET["id"]
-            if not is_in_cart(request.user, product_id):
-                add_to_cart(request.user, product_id)
-
+    request.session.pop('login_to_continue_to', None)
     args = {"item": fetchFullItem(request.GET["id"][:-1] if request.GET["id"][-1] == '/' else request.GET["id"])}
-    args["in_cart"] = is_in_cart(request.user, args["item"]["ID"]) if request.user.is_authenticated else False
-    return render(request, 'main_item_page.html', args)
+
+    args["in_cart"] = is_in_cart(request.user, args["item"]["ID"]) if request.user.is_authenticated and args[
+        "item"] is not None else False
+    if args["item"] == -1:
+        return HttpResponse("<h1>Error</h1><p>Item Does Not Exist</p>")
+    elif request.method == "GET":
+        return render(request, 'pages/main_item_page.html', args)
+    elif request.method == "POST":
+        if request.POST["type"] == "add_to_cart":
+            product_id = args["item"]["ID"]
+            if not request.user.is_authenticated:
+                request.session['login_to_continue_to'] = f'/item/?id={product_id}'
+                return redirect('/login/')
+            elif not args["in_cart"]:
+                add_to_cart(request.user, product_id)
+                return redirect(f'/item/?id={product_id}/')
+            else:
+                return HttpResponse("<h1>Error</h1><p>Item Already In Cart</p>")
+        else:
+            return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
+    else:
+        return HttpResponse("<h1>Error</h1><p>Bad Request</p>")
+
+
+# FIXME: Extremely important, validate if can checkout (stock, positive items, ....), maybe change method
+def checkout(request):
+    request.session.pop('login_to_continue_to', None)
+    if request.method == "GET":
+        return render(request, 'pages/checkout.html')
+    else:
+        return request
 
 
 # TODO: Extremely important, validate if can checkout (stock, positive items, ....)
 def checkout(request):
-    return render(request, 'checkout.html')
+    return render(request, 'pages/checkout.html')
 
 
 def profile(request):
+    request.session.pop('login_to_continue_to', None)
     if request.user.is_authenticated:
-        return HttpResponse("Profile")
+        return HttpResponse(
+            f"<h1>Profile</h1><h2>User ID:</h2><p>{request.user.id}</p><h2>Username:</h2><p>{request.user.username}</p>")
     else:
-        request.session['login_to_continue'] = True
+        request.session['login_to_continue_to'] = '/profile'
         return redirect('/login/')
 
 
 def logout(request):
+    request.session.pop('login_to_continue_to', None)
     logout_user(request)
     return redirect("/")
 
