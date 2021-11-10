c414004802e316b37243f41c20020c8872e90ed1
EtashTyagi <etash18@gmail.com>
2021-11-10 19:18:23 +0530

    fix test

--- Main/views.py
+++ Main/views.py
@@ -1,8 +1,12 @@
+from django.http import Http404
 from django.shortcuts import render
 
 
 def home(request):
     request.session.pop('login_to_continue_to', None)
+    if len(request.POST) > 0 or len(request.GET) > 0 or request.method == "POST":
+        return Http404("Wrong Request")
     return render(request, 'pages/home.html')
+
 
 all_views = {"home": home}
